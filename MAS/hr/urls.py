ff0835229e8c5359f246f162d984c4d057b802c3
Ahad48 <ahadhamirani62@gmail.com>
2021-03-28 19:43:00 +0530

    deleting files

--- MAS/hr/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("Add_Employee_Skill/", views.AddEmployeeSkillScoreView.as_view(), name="add_employee_skill"),
-    path("view_skill_chart/", views.generate_skill_chart, name='view_skill_chart'),
-    path("add_emp_form/", views.AddEmployeesView.as_view(), name="add_emp_form"),
-]
--- MAS/operations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("show_gantt_chart/", views.show_gantt_chart, name='show_gantt_chart'),
-]
